78c08af3f1b8102d095547eb36ab8ce645f55b6e
space-nuko <24979496+space-nuko@users.noreply.github.com>
2023-05-09 11:23:36 -0500

    Improve LogEvent display

--- packages/nodes-events/src/LogEvent.ts
+++ packages/nodes-events/src/LogEvent.ts
@@ -1,4 +1,4 @@
-import { BuiltInSlotType, LGraphNode, LiteGraph, OptionalSlots, PropertyLayout, SlotLayout, Vector2 } from "@litegraph-ts/core"
+import { BuiltInSlotType, ITextWidget, LGraphNode, LiteGraph, OptionalSlots, PropertyLayout, SlotLayout, Vector2 } from "@litegraph-ts/core"
 
 export interface LogEventProperties extends Record<string, any> {
 }
@@ -21,8 +21,22 @@ export default class LogEvent extends LGraphNode {
 
     override size: Vector2 = [60, 30];
 
+    actionWidget: ITextWidget;
+    paramWidget: ITextWidget;
+    optionsWidget: ITextWidget;
+
+    constructor(title?: string) {
+        super(title)
+        this.actionWidget = this.addWidget("text", "Action", "", null, { multiline: true, max_length: 100 })
+        this.paramWidget = this.addWidget("text", "Param", "", null, { multiline: true, max_length: 100 })
+        this.optionsWidget = this.addWidget("text", "Opts", "", null, { multiline: true, max_length: 100 })
+    }
+
     override onAction(action: any, param: any, options: { action_call?: string }) {
         console.log("[LogEvent] Event received:", action, param, options);
+        this.actionWidget.value = JSON.stringify(action);
+        this.paramWidget.value = JSON.stringify(param);
+        this.optionsWidget.value = JSON.stringify(options);
     }
 }
 
