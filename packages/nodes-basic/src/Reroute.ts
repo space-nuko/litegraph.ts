a57af62d4648bc85b264329ff193c61d9ba84d6d
space-nuko <24979496+space-nuko@users.noreply.github.com>
2023-05-23 21:14:22 -0500

    Reroute needs wildcard type

--- packages/nodes-basic/src/Reroute.ts
+++ packages/nodes-basic/src/Reroute.ts
@@ -1,12 +1,12 @@
-import { LGraphNode, LiteGraph, SlotLayout, TitleMode } from "@litegraph-ts/core";
+import { ContextMenuItem, LGraphNode, LiteGraph, SlotLayout, TitleMode } from "@litegraph-ts/core";
 
 export default class Reroute extends LGraphNode {
     static slotLayout: SlotLayout = {
         inputs: [
-            { name: "", type: "" },
+            { name: "", type: "*" },
         ],
         outputs: [
-            { name: "", type: "" }
+            { name: "", type: "*" }
         ]
     }
 
@@ -16,6 +16,35 @@ export default class Reroute extends LGraphNode {
 
     override onExecute() {
         this.setOutputData(0, this.getInputData(0));
+    }
+
+    override getExtraMenuOptions(_, options: ContextMenuItem[]): ContextMenuItem[] | null {
+        const canSplice = this.getInputLink(0) != null && this.getOutputLinks(0).length > 0;
+
+        options.push({
+            content: "Splice & Remove",
+            disabled: !canSplice,
+            callback: () => {
+                const inputLink = this.getInputLink(0);
+                const outputLinks = this.getOutputLinks(0);
+                if (!inputLink || !outputLinks)
+                    return;
+
+                const inputNode = this.graph.getNodeById(inputLink.origin_id)
+                this.graph.removeLink(inputLink.id);
+
+                for (const outputLink of outputLinks) {
+                    const outputNode = this.graph.getNodeById(outputLink.target_id)
+                    this.graph.removeLink(outputLink.id);
+
+                    inputNode.connect(inputLink.origin_slot, outputNode, outputLink.target_slot);
+                }
+
+                this.graph.remove(this);
+            }
+        })
+
+        return null;
     }
 }
 
