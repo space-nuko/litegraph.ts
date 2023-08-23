035cd0a9c1f4bfd952ce3ec3c09f96ae3e744a67
space-nuko <24979496+space-nuko@users.noreply.github.com>
2023-08-23 13:22:56 -0500

    Add mini bundle

--- build.sh
+++ build.sh
@@ -7,4 +7,11 @@ mkdir -p out
 cp packages/core/dist/litegraph-ts.es.js out/litegraph.core.js
 echo "Wrote output to out/litegraph.core.js"
 
-cp out/litegraph.core.js E:/ComfyUI/web/lib
+pushd bundles/mini
+pnpm build
+popd
+mkdir -p out
+cp bundles/mini/dist/litegraph-ts.mini.es.js out/litegraph.mini.js
+
+# cp out/litegraph.core.js E:/ComfyUI/web/lib
+cp out/litegraph.mini.js E:/ComfyUI/web/lib/litegraph.core.js
